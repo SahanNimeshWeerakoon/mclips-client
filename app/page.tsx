3a7bf033d5aa90140604e2bcc923823960d84ab6
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2025-10-07 19:26:22 +0530

    test

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
     <section className="container mx-auto py-10 px-5">
       <Search />
       <Videos />
-      <p>kapan mage</p>
+      <p>kapan mage walla</p>
       {/* <VideoCropper videoUrl="/video.mp4" /> */}
     </section>
   );
