8c9781fc63d40c34fc67751b25c22d58479c207f
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2025-12-14 13:43:32 +0530

    add square icons in crop box

--- components/video/videoCropper.tsx
+++ components/video/videoCropper.tsx
@@ -21,7 +21,12 @@ export default function VideoCroppper({ videoUrl }: Props) {
         <div className="overlay"></div>
 
         <CropBox>
-          <div className="w-full h-full border shadow-[0_0_0_9999px_rgba(0,0,0,0.25)]">test</div>
+          <div className="relative w-full h-full border border-white shadow-[0_0_0_9999px_rgba(0,0,0,0.25)]">
+            <span className="tl absolute w-3 h-3 bg-white"></span>
+            <span className="tr absolute w-3 h-3 bg-white"></span>
+            <span className="bl absolute w-3 h-3 bg-white"></span>
+            <span className="br absolute w-3 h-3 bg-white"></span>
+          </div>
         </CropBox>
       </div>
   );
